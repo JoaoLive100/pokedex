a35ed3c341ea0c4a6352b810a0b427700218aac7
João Rodrigues Parra <j-r-p@outlook.com>
2023-05-04 15:36:43 -0300

    (04/05/23) - Resolvendo bugs para deploy no vercel

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
-const API = environment.api;
+//const API = environment.api;
 
 @Injectable({
   providedIn: 'root'
